7a975e16a475c182c46bd2ec696201e261aecfca
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2025-01-21 02:12:19 +0900

    feat(アプリ) : VSCodeでFlutterの空アプリを生成した

---
+++ application/kimera_app/README.md
@@ -0,0 +1,3 @@
+# kimera_app
+
+A new Flutter project.
---
+++ application/kimera_app/android/app/src/main/kotlin/com/example/kimera_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kimera_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
